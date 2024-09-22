e1ef25b544556a3d4465010197fc4fb17c4e6c92
Titan Chen <titanchen@yahoo.com>
2024-09-22 19:20:55 -0400

    Added purchase and purchase/paymentEntry pages

--- big-foot-shoes/src/components/paymentEntry.js
+++ big-foot-shoes/src/components/paymentEntry.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const PaymentEntry = () => {
+    const location = useLocation();
+    
+    console.log('paymentEntry: ', location);
+
     return (
-        <div></div>
+        <div>
+            <h1>
+                Shoe 1 {location.state.order.buyQuantity[0]}
+                <br/>
+                Shoe 2 {location.state.order.buyQuantity[1]}
+            </h1>
+        </div>
     );
 };
 
--- big-foot-shoes/src/components/purchase.js
+++ big-foot-shoes/src/components/purchase.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Purchase = () => {
+    const [order, setOrder] = useState({
+        buyQuantity: [0,0,0,0,0], credit_card_number: '', expir_date: '', cvvCode: '',
+        card_holder_name: '', address_1: '', address_2: '', city: '', state: '', zip: ''
+    });
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        console.log('order: ' + order.buyQuantity)
+        navigate('/purchase/paymentEntry', {state: {order: order}});
+    }
+
+
     return (
-        <div></div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Shoe 1</label>
+                <input
+                    type='number'
+                    required
+                    onChange={(e) => {order.buyQuantity[0] = e.target.value;}}
+                />
+                <br/>
+                <label>Shoe 2</label>
+                <input
+                    type='number'
+                    required
+                    onChange={(e) => {order.buyQuantity[1] = e.target.value;}}
+                />
+                <br/>
+                <button className='button'>Pay</button>
+            </form>
+        </div>
     );
 };
 
